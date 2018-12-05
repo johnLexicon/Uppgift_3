2be000a716adff8fa67e11e9f390cce52aac095b
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-05 16:13:03 +0100

    Added nested static class to handle Person objects.

--- Uppgift_3_OOP/Person.cs
+++ Uppgift_3_OOP/Person.cs
@@ -8,17 +8,47 @@ namespace Uppgift_3_OOP
         private int age;
         private string fname;
         private string lname;
-        private int height;
-        private float weight;
+        private double height;
+        private double weight;
         #endregion
 
         #region Properties
         public int Age { get => age; set => age = value; }
         public string Fname { get => fname; set => fname = value; }
         public string Lname { get => lname; set => lname = value; }
-        public int Height { get => height; set => height = value; }
-        public float Weight { get => weight; set => weight = value; }
+        public double Height { get => height; set => height = value; }
+        public double Weight { get => weight; set => weight = value; }
         #endregion
-        
+
+        private Person() { }
+
+        private Person(int age, string fname, string lname, double height, double weight)
+        {
+            Age = age;
+            Fname = fname;
+            Lname = lname;
+            Height = height;
+            Weight = weight;
+        }
+
+        public static class PersonHandler
+        {
+
+            public static Person CreatePerson(int age, string fname, string lname, double height, double weight)
+            {
+                return new Person(age, fname, lname, height, weight);
+            }
+
+            public static Person CreatePerson()
+            {
+                return new Person();
+            }
+
+            public static void SetAge(Person pers, int age)
+            {
+                pers.Age = age;
+            }
+
+        }
     }
 }
