86df508947a4c88ad1a5343eafaf6a7d1d093ae0
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-05 06:39:15 +0100

    Comments for answer 3.1

--- Uppgift_3_OOP/Program.cs
+++ Uppgift_3_OOP/Program.cs
@@ -8,6 +8,9 @@ namespace Uppgift_3_OOP
         {
             Person p1 = new Person();
 
+            //Cannot access the fields in the class Person due to their access modifier is private e.g:
+            //p1.age = 10;
+            //var fname = p1.fname;
         }
     }
 }
