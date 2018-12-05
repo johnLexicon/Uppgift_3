14cd63576b82730bf9964ae39434ad14dbc8beb3
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-05 04:32:04 +0100

    First commit

---
+++ Uppgift_3_OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Uppgift_3_OOP
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
