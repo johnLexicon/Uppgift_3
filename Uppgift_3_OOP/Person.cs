a5a11515c7230ac0a031825aa598dceef32b5060
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-05 05:46:14 +0100

    Added Person class

---
+++ Uppgift_3_OOP/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_3_OOP
+{
+    class Person
+    {
+
+        #region Fields
+        private int age;
+        private string fname;
+        private string lname;
+        private int height;
+        private float weight;
+        #endregion
+
+        #region Properties
+        public int Age { get => age; set => age = value; }
+        public string Fname { get => fname; set => fname = value; }
+        public string Lname { get => lname; set => lname = value; }
+        public int Height { get => height; set => height = value; }
+        public float Weight { get => weight; set => weight = value; }
+        #endregion
+        
+    }
+}
